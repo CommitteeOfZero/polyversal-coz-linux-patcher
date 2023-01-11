7d70a204a8ba8f39ec8654a1773ef6afc2ae4803
Arcana <untruth-brink@mail.plural.moe>
2023-01-11 10:50:49 -0500

    Make an otherwise useless conditional statement... useful... (#1)
    Make an otherwise useless conditional... useful
    Also switch to double-square bracket notation for conditionals

--- patch-sciadv.sh
+++ patch-sciadv.sh
@@ -3,13 +3,13 @@
 # Set up and install Protontricks
 flatpak install com.github.Matoking.protontricks
 flatpak override --user --filesystem="~/Downloads" com.github.Matoking.protontricks
-if [ "$3" == "deck" ]; then
+if [[ "$4" == "deck" ]]; then
   flatpak override --user --filesystem=/run/media/mmcblk0p2/
 fi
 
 # Patch the game
 protontricks="flatpak run com.github.Matoking.protontricks"
-if [ "$4" == "deck" ]; then
+if [[ "$4" == "deck" ]]; then
   $protontricks -c "cd ~/Downloads/$2 && STEAM_COMPAT_MOUNTS=/run/media/mmcblk0p2 wine $3" $1
 else
   $protontricks -c "cd ~/Downloads/$2 && wine $3" $1
